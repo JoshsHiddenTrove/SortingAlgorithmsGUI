eb37b49c52b6e40d83d475384dea44a22fce6cbc
Josh <Tacofuntrain@gmail.com>
2020-06-06 17:30:04 -0600

    setting up reposityory

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World!</h1>
+        <h1>A gui for a variety of sorting algorithms will be on this page eventually!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
